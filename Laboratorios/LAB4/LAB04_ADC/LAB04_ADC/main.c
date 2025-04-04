64960561d488c9e0cd8b52b1c00ff624989c405a
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2025-04-04 15:55:19 -0600

    Configuraciones
    COnfiguración de timer para el multiplexeo y configuración o inicialización del ADC

--- Laboratorios/LAB4/LAB04_ADC/LAB04_ADC/main.c
+++ Laboratorios/LAB4/LAB04_ADC/LAB04_ADC/main.c
@@ -6,8 +6,13 @@
  */ 
 
 /*********Librerias*********/
+#define F_CPU 16000000UL
 #include <avr/io.h>
-
+#include <avr/interrupt.h>
+uint8_t pb=0;			//Estados de botones
+uint8_t contador=0;		//Contador para display
+uint8_t top=0xFF;		//Tope de overflow
+uint8_t bottom=0;		//Tope de overflow
 /*********Librerias*********/
 
 
