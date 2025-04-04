a4cfa60ca3620b455c6f8f9381bed55a0ba7658e
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2025-04-04 14:01:03 -0600

    Inicio de Lab4
    Código base del prelab4

---
+++ Laboratorios/LAB4/LAB04_ADC/LAB04_ADC/main.c
@@ -0,0 +1,37 @@
+/*
+ * LAB04_ADC.c
+ *
+ * Created: 4/4/2025 11:21:39 AM
+ * Author : Juan Rodriguez
+ */ 
+
+/*********Librerias*********/
+#include <avr/io.h>
+
+/*********Librerias*********/
+
+
+/*********Prototipos de funciones*********/
+
+/*********Prototipos de funciones*********/
+
+
+/*********main*********/
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+/*********main*********/
+
+
+/*********Subrutinas NON Interrupts*********/
+
+/*********Subrutinas NON Interrupts*********/
+
+
+/*********Subrutinas Interrupts*********/
+
+/*********Subrutinas NON Interrupts*********/
