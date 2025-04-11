c71ab31e99096287857bdc641dd021323105791a
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2025-04-11 12:23:27 -0600

    Realizando calculos de PWM
    Duty cycle para 0°

--- Laboratorios/LAB5/Prelab5/Prelab5/main.c
+++ Laboratorios/LAB5/Prelab5/Prelab5/main.c
@@ -23,6 +23,8 @@ void initPWM1();
 int DutyCycle(uint8_t lec_ADC);
 
 uint8_t valorADC=0;
+uint8_t limit_inf=101;
+uint16_t rango=307;
 
 int main()
 {
@@ -38,11 +40,9 @@ void setup(){
 	cli();
 	CLKPR = (1<< CLKPCE);
 	CLKPR |= (1<<CLKPS2);	//1Mhz
-	
-	DDRB = (1<<PORTB1) | (1<<PORTB2)| (1<<PORTB3);		//PB1 - PB3 como salidas
-	PORTB= 0x00;		//conduce 0 lógico
-	
-	UCSR0B = 0;
+	DDRB |= (1 << PORTB1);	//PB1 como salida
+		
+	UCSR0B = 0;				//Comunicación serial
 	initADC();
 	initPWM1();
 	sei();
@@ -54,29 +54,29 @@ void initADC(){
 	
 	ADCSRA = 0;
 	ADCSRA |= (1<<ADEN)|(1<<ADIE)|(1<<ADPS1)|(1<<ADPS0); //Habilitación de ADC y interrupción - Prescaler para 125 Khz 
+	ADCSRA |= (1<<ADSC); //Iniciar conversión
 }
 
-void initPWM1(){
-								
+void initPWM1(){				
 	TCCR1A = 0;
 	TCCR1B = 0;
-	
 	TCCR1A |= (1<<COM1A1) ; //Modo non-invertido
 	//Modo FAST PWM con TOP en ICR1
 	TCCR1A |= (1<<WGM11);
 	TCCR1B |= (1<<WGM12)|(1<<WGM13);
 	TCCR1B |= (1<<CS11);	//Prescaler de 8
-	
-	ICR1= 6553; //TOP
+	ICR1= 2499; //TOP
+	OCR1A = 62;	//Duty cycle 2.5%
 }
 
 int DutyCycle(uint8_t lec_ADC){
-	
-	return lec_ADC;
+	return (lec_ADC*307UL/255+limit_inf);
 }
 
 //************Interrupciones************
+/*
 ISR(ADC_vect){
 	valorADC = ADCH;
+	OCR1A=DutyCycle(valorADC);
 	ADCSRA |= (1<<ADSC);
-}
+}*/
