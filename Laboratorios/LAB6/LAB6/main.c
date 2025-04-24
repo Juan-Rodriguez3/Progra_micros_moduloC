3ac76a003e53180214446f5f30412b1b205cbbfd
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2025-04-24 10:22:20 -0600

    Inicio de Prelab 6

---
+++ Laboratorios/LAB6/LAB6/main.c
@@ -0,0 +1,18 @@
+/*
+ * LAB6.c
+ *
+ * Created: 4/24/2025 10:20:40 AM
+ * Author : juana
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
