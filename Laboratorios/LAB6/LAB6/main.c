26c017af47ed29ead2633a17992ef19d226bd0d4
Juan-Rodriguez3 <126404166+Juan-Rodriguez3@users.noreply.github.com>
2025-04-24 11:29:41 -0600

    Configuración de recepción y tranmisión
    El nano esta configurado para leer y escribir, pero no se han creado funciones para ello.

--- Laboratorios/LAB6/LAB6/main.c
+++ Laboratorios/LAB6/LAB6/main.c
@@ -1,18 +1,59 @@
 /*
- * LAB6.c
+ * LAB04_ADC.c
  *
- * Created: 4/24/2025 10:20:40 AM
- * Author : juana
+ * Created: 24/04/2025
+ * Author : Juan Rodriguez
+	Laboratorio 6
+	comunicación serial UART
  */ 
 
+/*********Librerias*********/
+#define F_CPU 16000000UL
 #include <avr/io.h>
+#include <avr/interrupt.h>
+	
+/*********Librerias*********/
+void setup(void);
+void initUART(void);
+
+/*********Prototipos de funciones*********/
+
+/*********Prototipos de funciones*********/
 
 
+/*********main*********/
 int main(void)
 {
-    /* Replace with your application code */
+    setup(void);
     while (1) 
     {
+		
     }
 }
+/*********main*********/
+
+
+/*********Subrutinas NON Interrupts*********/
+void setup(void){
+	cli();
+	initUART(void);
+	sei();
+}
+
+void initUART(void){
+	//Configurar los pines PD1 Tx y PD0 Rx
+	DDRD=0;
+	DDRD |= (1<<PORTD1);	//PD1 única salida
+	UCSR0A = 0;
+	UCSR0B = 0;
+	UCSR0C = 0;
+	
+}
+
+/*********Subrutinas NON Interrupts*********/
+
+
+/*********Subrutinas Interrupts*********/
+
+/*********Subrutinas NON Interrupts*********/
 
